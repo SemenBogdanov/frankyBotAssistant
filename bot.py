7715b17b55f9551b373ad9fa1754b79238572210
Sam <41944436+SemenBogdanov@users.noreply.github.com>
2019-09-09 23:29:31 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,16 @@
-import telebot
 import os
+import telebot
 
-token=os.environ.get('Bottoken')
+token = os.environ.get('Bottoken')
 bot = telebot.TeleBot(str(token))
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
 	bot.reply_to(message, "Привет! Стартанули!")
 
-@bot.message_handler(context_type=[text])
+@bot.message_handler(context_type=['text'])
 def function_name(message):
-	bot.send_message(message.chat.id,"Наконец-то простое сообщение!")
+	bot.send_message(message.chat.id, "Наконец-то простое сообщение!")
 	
 bot.polling()
+
