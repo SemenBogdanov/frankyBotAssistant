7375ff78d1d54632c7dd75680f5c7ad35debc38f
Sam <41944436+SemenBogdanov@users.noreply.github.com>
2019-09-10 00:11:23 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,16 +1,16 @@
 import os
 import telebot
 
-token = os.environ.get('Bottoken')
+token=os.environ.get('Bot-token')
 bot = telebot.TeleBot(str(token))
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
 	bot.reply_to(message, "Привет! Стартанули!")
 
-@bot.message_handler(context_type=['text'])
+@bot.message_handler(content_types=['text'])
 def function_name(message):
 	bot.send_message(message.chat.id, "Наконец-то простое сообщение!")
-	
-bot.polling()
 
+
+bot.polling()
