61e5334ddcb825cf546e01019b24c4847459abe4
Sam <41944436+SemenBogdanov@users.noreply.github.com>
2019-09-10 13:03:06 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,9 +4,14 @@ import telebot
 token=os.environ.get('BOT_TOKEN')
 bot = telebot.TeleBot(str(token))
 
+greetings='''Привет! Ну что? Поехали по-тихонечку?!
+меня зовут Фрэнк! Рад буду напоминать тебе о том, что ты всегда забывал. 
+А когда мой разработчик добавит мне базу данных для памяти, я вообще буду крут!
+Как тебя зовут?'''
+
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-	bot.reply_to(message, "Привет! Стартанули!")
+	bot.reply_to(message, greetings)
 
 @bot.message_handler(content_types=['text'])
 def function_name(message):
