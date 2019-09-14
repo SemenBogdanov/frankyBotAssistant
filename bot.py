11db2d25f9c3c4412e7099f775863640fc0a7280
Sam <41944436+SemenBogdanov@users.noreply.github.com>
2019-09-14 11:00:29 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,9 @@
 import os
 import telebot
 
+global functions, coffee
+functions = ("функции","функция", "функций","функц")
+
 token=os.environ.get('BOT_TOKEN')
 bot = telebot.TeleBot(str(token))
 
@@ -13,9 +16,38 @@ greetings='''Привет! Ну что? Поехали по-тихонечку?!
 def send_welcome(message):
 	bot.reply_to(message, greetings)
 
+def welcome_func(chat_id):
+	markup=InlineKeyboardMarkup()
+	markup.row_width=4
+	markup.add(InlineKeyboardButton("Посоветовать хорошую кофейню", callback_data="get_coffee_place"))
+	markup.add(InlineKeyboardButton("Погода", callback_data="get_weather"),
+		InlineKeyboardButton("Курс валют", callback_data="course_exchange"))
+	markup.add(InlineKeyboardButton("Запрограммировать словарь", callback_data="programming_voc"))
+	return markup
+
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_query(call):
+    if call.data == "get_weather":
+        bot.answer_callback_query(call.id, "выбрана погода")
+    elif call.data == "get_coffee_place":
+        bot.answer_callback_query(call.id, "ДаркСайд")
+    elif call.data == "course_exchange":
+        bot.answer_callback_query(call.id, "выбран курс валют")
+    elif call.data == "programming_voc":
+        bot.answer_callback_query(call.id, "запрограммировать словарь")  
+
 @bot.message_handler(content_types=['text'])
 def function_name(message):
-	bot.send_message(message.chat.id, "Наконец-то простое сообщение!")
+	for i in range(len(functions)):
+		if functions[i] in message.text:
+			bot.send_message(message.chat.id, "Вот что я могу!", reply_markup=welcome_func(message.chat.id))
+			return;
+		else :
+			continue	
+	i+=1
+	bot.send_message(message.chat.id, "Попробуй спросить меня о функциях!")
 
 
 bot.polling()
