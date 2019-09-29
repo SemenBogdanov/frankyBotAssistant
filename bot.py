7ae8b7cd6880ea1c6d344b03d3eedb3a0c8b894c
Sam <41944436+SemenBogdanov@users.noreply.github.com>
2019-09-29 11:56:14 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import os
 import telebot
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+from rpoInfo import getRpoInfo
 
 global functions, coffee
 functions = ("функции","функция", "функций","функц")
@@ -8,47 +9,77 @@ functions = ("функции","функция", "функций","функц")
 token=os.environ.get('BOT_TOKEN')
 bot = telebot.TeleBot(str(token))
 
-greetings='''Привет! Ну что? Поехали по-тихонечку?!
-меня зовут Фрэнк! Рад буду напоминать тебе о том, что ты всегда забывал. 
-А когда мой разработчик добавит мне базу данных для памяти, я вообще буду крут!
-Как тебя зовут?'''
+greetings='''Привет! Спроси о функциях!'''
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
 	bot.reply_to(message, greetings)
 
-def welcome_func(chat_id):
+def get_Rpo(call):
+	msg=bot.send_message(call.message.chat.id, "Введите номер отправления:")
+	bot.register_next_step_handler(msg, get_Rpo2)
+
+def get_Rpo2(msg):
+	answer=getRpoInfo(msg.text)
+	bot.send_message(msg.chat.id, answer)
+
+def recVoc(msg):
+	#file=open('vocablulary.txt', a)
+	bot.send_message(msg.chat.id, msg.text)
+
+def programming_voc(call):
+	bot.answer_callback_query(call.id, "запрограммировать словарь") 
+	msg=bot.send_message(call.message.chat.id, "Введите сообщение для записи:")
+	bot.register_next_step_handler(msg, recVoc)
+	#
+
+def get_coffee_place(call):
+	bot.answer_callback_query(call.id, "Посоветовать кофейню!")
+	file=open('descCoffee.txt')
+	cid=call.message.chat.id
+	t=file.read()
+	bot.send_message(cid, t)
+	bot.send_message(cid, "Может что-нибудь ещё?", reply_markup=welcome_func())
+	file.close()
+	#return t
+	pass
+
+def welcome_func():
 	markup=InlineKeyboardMarkup()
-	markup.row_width=4
-	markup.add(InlineKeyboardButton("Посоветовать хорошую кофейню", callback_data="get_coffee_place"))
+	markup.row_width=3
+	markup.add(InlineKeyboardButton("Посоветовать хорошую кофейню!", 
+		callback_data="get_coffee_place"))
 	markup.add(InlineKeyboardButton("Погода", callback_data="get_weather"),
-		InlineKeyboardButton("Курс валют", callback_data="course_exchange"))
+			InlineKeyboardButton("Курс валют", callback_data="course_exchange"))
 	markup.add(InlineKeyboardButton("Запрограммировать словарь", callback_data="programming_voc"))
+	markup.add(InlineKeyboardButton("Отслеживание ПочтаРоссии", callback_data="get_Rpo"))
 	return markup
-
-
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_query(call):
     if call.data == "get_weather":
-        bot.answer_callback_query(call.id, "выбрана погода")
+    	bot.answer_callback_query(call.id, "Выбрана погода")
     elif call.data == "get_coffee_place":
-        bot.answer_callback_query(call.id, "ДаркСайд")
+        get_coffee_place(call)
     elif call.data == "course_exchange":
         bot.answer_callback_query(call.id, "выбран курс валют")
     elif call.data == "programming_voc":
-        bot.answer_callback_query(call.id, "запрограммировать словарь")  
+        programming_voc(call)
+    elif call.data == "get_Rpo":
+    	get_Rpo(call)     
 
 @bot.message_handler(content_types=['text'])
 def function_name(message):
 	for i in range(len(functions)):
 		if functions[i] in message.text:
-			bot.send_message(message.chat.id, "Вот что я могу!", reply_markup=welcome_func(message.chat.id))
+			bot.send_message(message.chat.id, 
+				"Вот что я могу!", reply_markup=welcome_func())
 			return;
 		else :
 			continue	
 	i+=1
 	bot.send_message(message.chat.id, "Попробуй спросить меня о функциях!")
 
+print("бот запущен!")
 
 bot.polling()
