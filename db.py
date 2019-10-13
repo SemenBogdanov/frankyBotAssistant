2b6c2ab71b8b9fd5879836623d19afaba59d2cae
Sam <41944436+SemenBogdanov@users.noreply.github.com>
2019-10-13 12:31:57 +0300

    Update db.py

--- db.py
+++ db.py
@@ -5,16 +5,30 @@ DATABASE_URL = os.environ.get('DATABASE_URL')
 
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
-#select
-sql = 'SELECT * FROM memo'
- 
-try:
-    cur = conn.cursor()
-    # cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor) # by column name
-    cur.execute(sql)
-    data = cur.fetchall()
-except psycopg2.Error as err:
-    print("Query error: {}".format(err))
-    
-print(data)
+def checkUser(chatId):
+	sql="select username from usersbot as u where u.messagechatid='" + str(chatId) + "'"
+	try:
+	    cur = conn.cursor()
+	    # cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor) # by column name
+	    cur.execute(sql)
+	    data = cur.fetchall()
+	    return data
+	    
+	except psycopg2.Error as err:
+	    print("Query error: {}".format(err))
+
+def addUser(ans):
+	print(ans.chat.id)
+	sql="insert into usersbot (username, messagechatid, tlgusername) \
+	values ('" + ans.text + "','" + str(ans.chat.id) + "','" + str(ans.chat.username) + "')"
+	print(sql)
+	try:
+	    cur = conn.cursor()
+	    # cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor) # by column name
+	    cur.execute(sql)
+	    conn.commit()
+	    return True
+	except psycopg2.Error as err:
+	    print("Query error: {}".format(err))
+		
 
